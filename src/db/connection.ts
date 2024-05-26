86561097431be5140362621fe48a70fe890aa510
joaovcostas <jvc22102003@gmail.com>
2024-05-26 19:52:29 -0300

    fix: apply eslint rules

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 
 import * as schema from './schema'
 
@@ -8,5 +8,5 @@ import { env } from '../env'
 const connection = postgres(env.DATABASE_URL)
 
 export const db = drizzle(connection, {
-    schema,
+  schema,
 })
